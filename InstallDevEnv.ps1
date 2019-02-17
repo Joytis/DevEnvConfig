67edbaba204c560e49adbd31a809193e5d717d06
Joytis <clarkchambers94@gmail.com>
2019-02-17 15:37:34 -0600

    Added a few more things

---
+++ InstallDevEnv.ps1
@@ -0,0 +1,28 @@
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install basic dev env stuff. 
+choco install firefox
+choco install sublimetext3
+choco install sourcetree
+
+# Install Applications
+choco install visualstudiocode -y
+choco install GoogleChrome -y
+choco install putty -y
+choco install ccleaner -y
+choco install kdiff3 -y
+choco install unity-hub  -y
+choco install discord  -y
+
+# Install Utilities
+choco install dropbox -y
+choco install googledrive  -y
+choco install procmon  -y
+choco install windirstat  -y
+choco install git --params "/GitAndUnixToolsOnPath" -y
+choco install 7zip -y
+choco install hxd -y
+choco install nodejs -y
+choco install rust -y
+
